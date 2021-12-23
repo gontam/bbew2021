0f43cef9f891503c4a857b2252bbfa2ab0ccda41
gontam <gonta@technikum-wien.at>
2021-12-23 17:52:50 +0100

    My Example 1 - after changes

--- myexample1.py
+++ myexample1.py
@@ -1,20 +1,18 @@
 def check_palindrome(string_line):
-    if string_line==string_line[::-1]:
-        return True
-    else:
-        return False
+    return string_line == string_line[::-1]
 
-file =open('palindrome_check.txt', 'r')
-file_lines = file.readlines()
-file_lines_strip = []
+
+FILE_NAME = 'palindrome_check.txt'
+
+with open(FILE_NAME) as file:
+    file_lines = file.read().splitlines()
+
+new_file = []
+
 for line in file_lines:
-    file_lines_strip.append(line.strip())
-new_file= []
-
-for line in file_lines_strip:
     checked = check_palindrome(line)
     new_file.append(line + ' ' + str(checked) + '\n')
 
-file = open('palindrome_check.txt', 'w')
+file = open(FILE_NAME, 'w')
 file.writelines(new_file)
 file.close()
